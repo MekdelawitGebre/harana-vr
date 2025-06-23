986674503bd7423eae8046a54aae578dd55fdf1c
Mekdelawit Gebre <136165989+MekdelawitGebre@users.noreply.github.com>
2025-06-23 09:33:38 +0300

    feat(pages): create home page with initial layout

---
+++ src/pages/Index.tsx
@@ -0,0 +1,22 @@
+
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import FeaturesSection from "@/components/FeaturesSection";
+import DemoSection from "@/components/DemoSection";
+import VisitOromiaSection from "@/components/VisitOromiaSection";
+import FooterSection from "@/components/FooterSection";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <HeroSection />
+      <AboutSection />
+      <FeaturesSection />
+      <DemoSection />
+      <VisitOromiaSection />
+      <FooterSection />
+    </div>
+  );
+};
+
+export default Index;
